b92c70275c7474aef264bf78b114c0a23b08f3bf
Christophe Benz <christophe.benz@data.gouv.fr>
2015-03-11 10:39:58 +0100

    VariableDefinition type is immutable

--- src/variable_definitions.jl
+++ src/variable_definitions.jl
@@ -20,7 +20,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-type VariableDefinition
+immutable VariableDefinition
   formula
   name::String
   entity_definition::EntityDefinition
